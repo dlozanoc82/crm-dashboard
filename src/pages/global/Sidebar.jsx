e0102c3d6f77649bfb03d28aec9dbc6b0bdc3693
DCODER <dlozanoc862@gmail.com>
2024-07-21 17:37:43 -0500

    Start project

---
+++ src/pages/global/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useState } from "react"
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css'
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactOutlinedIcon from "@mui/icons-material/ContactOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimeLineOutlinedIcon from "@mui/icons-material/TimeLineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+
+
+
+const Sidebar = () => {
+  return (
+    <div>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
